0ed607cbefe749cad20fa81ae84074a835644f40
dmatos <dmatos88@gmail.com>
2024-02-17 22:13:33 -0300

    - Responsive main html block

--- intraday-charts/src/shared/main/AppMain.tsx
+++ intraday-charts/src/shared/main/AppMain.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Box} from "@mui/material";
+import {ISeriesApi} from "lightweight-charts";
 
-export interface AppMainProps{
-    charts: HTMLElement[]|null
+export interface ChartCardProps{
+    elementId: string,
+    series: ISeriesApi<any>,
 }
 
-export const AppMain:React.FC<AppMainProps> = ({charts}) => {
+export interface AppMainProps{
+    callbackFn: (ref:HTMLElement|null) => void,
+    width: number,
+    height: number
+}
+
+export const AppMain:React.FC<AppMainProps> = (appMainProps:AppMainProps) => {
+
+    const boxRef = useRef<HTMLElement>(null);
+
+    useEffect(()=> {
+        appMainProps.callbackFn(boxRef.current);
+        return () => {
+            if(!!boxRef.current){
+                boxRef.current.childNodes.forEach((n) => {
+                    boxRef.current?.removeChild(n);
+                })
+            }
+        }
+    })
+
     return (
         <Box id={"chartBox"}
-            display={"grid"}
+             display={"grid"}
+             ref ={boxRef}
+             height={appMainProps.height}
+             width={appMainProps.width}
         >
         </Box>
     )
--- intraday-charts/src/shared/main/AppMainContainer.tsx
+++ intraday-charts/src/shared/main/AppMainContainer.tsx
@@ -1,35 +1,52 @@
 import {AppMain, AppMainProps} from "./AppMain";
 import {createChart, UTCTimestamp} from "lightweight-charts";
+import React, {useCallback, useEffect, useState} from "react";
 
 export const AppMainContainer = () =>{
+    const [data, setData] = useState(
+        [
+            { time: 1554967140 as UTCTimestamp, value: 80.01 },
+            { time: 1554967200 as UTCTimestamp, value: 80.02 },
+            { time: 1554967260 as UTCTimestamp, value: 80.03 }
+        ]
+    )
 
-    const setupMockChart = () => {
-        let chartBox = document.getElementById("chartBox");
+    const setupMockChart = useCallback( (chartBox:HTMLElement|null) => {
         if(!!chartBox){
-            const chart = createChart(chartBox, { width: 400, height: 300,
-                localization: {
-                    timeFormatter: (timestamp:UTCTimestamp) => {
-                        return new Date(timestamp * 1000).toISOString();
+            let chart = createChart(chartBox,
+                {
+                    localization: {
+                        timeFormatter: (timestamp:UTCTimestamp) => {
+                            return new Date(timestamp * 1000).toISOString();
+                        },
                     },
-                },
-            });
+                });
             const lineSeries = chart.addLineSeries();
-            lineSeries.setData([
-                { time: 1554967140 as UTCTimestamp, value: 80.01 },
-                { time: 1554967200 as UTCTimestamp, value: 80.02 },
-                { time: 1554967260 as UTCTimestamp, value: 80.03 }
-            ]);
+            lineSeries.setData(data);
             chart.timeScale().fitContent();
         }
+    }, [data]);
 
+    const getAppMainProps = (): AppMainProps => {
+        return {
+            callbackFn: setupMockChart,
+            width: window.innerWidth,
+            height: Math.floor(window.innerHeight  * 90 / 100)
+        }
     }
 
+    const [appMainProps, setAppMainProps] = useState<AppMainProps>(getAppMainProps());
 
-    let appMainProps:AppMainProps = {
-        charts: []
-    };
+    const windowResizeListener = () => {
+       setAppMainProps(getAppMainProps());
+    }
 
-    setupMockChart();
+    useEffect( () => {
+        window.addEventListener('resize', windowResizeListener, false);
+        return () => {
+            window.removeEventListener('resize', windowResizeListener, false);
+        }
+    },[]);
 
     return (
         <AppMain {...appMainProps}/>
